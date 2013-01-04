562a1304f7313e5445bbbeb0802199d110f771d8
Luca Guidi <me@lucaguidi.com>
2013-01-04 12:48:13 +0100

    Lazy evaluation of controller name

--- lib/lotus/action.rb
+++ lib/lotus/action.rb
@@ -1,17 +1,21 @@
 module Lotus
   class Action
     def initialize(options)
-      @controller = constantize(options.fetch(:controller))
-      @action     = options.fetch(:action) {|e| :index }
+      @controller_name = options.fetch(:controller)
+      @action          = options.fetch(:action) {|e| :index }
     end
 
     def call(env)
-      @controller.new(env).call(@action)
+      controller.new(env).call(@action)
     end
 
     private
-    def constantize(controller)
-      Object.const_get "#{ controller.to_s.capitalize }Controller"
+    def controller
+      @controller ||= constantize_controller_name
+    end
+
+    def constantize_controller_name
+      Object.const_get "#{ @controller_name.to_s.capitalize }Controller"
     end
   end
 end
